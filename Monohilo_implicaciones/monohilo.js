aed07e3c9826f3abbd4046e5a826c085fa9fb831
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-03-28 17:28:28 -0500

    inicio

---
+++ Monohilo_implicaciones/monohilo.js
@@ -0,0 +1,13 @@
+console.log('Hola Mundo');
+
+let i = 0;
+setInterval(function() { 
+    console.log(i);
+    i++;
+
+    if(i===10){
+        var a = 3 +z;
+    }
+}, 100);
+
+console.log('Segunda instruccion');
